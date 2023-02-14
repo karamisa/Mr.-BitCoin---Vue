76dbfb94d7f6c367b6c97f86320a03fd56f577a0
Karam Isa <karam_isa@icloud.com>
2023-02-14 19:43:50 +0200

    adding dist subtree

--- src/cmps/ui/delete-btn.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <button class="rounded-md bg-red-500 p-2 text-white">Delete</button>
-</template>
-
-<script>
-export default {};
-</script>
--- src/cmps/ui/spinner.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div class="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-amber-400"></div>
-  </template>
-  
-  <script>
-  export default {
-	name: 'spinner',
-  }
-  </script>
