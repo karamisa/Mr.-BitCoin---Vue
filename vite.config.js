053dd3824fc240882ba17f7204b883832f5ff2d8
Karam Isa <karam_isa@icloud.com>
2023-02-15 16:17:18 +0200

    code cleanups various

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
-// https://vitejs.dev/config/
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Mr.-BitCoin---Vue/' : '/',
-}
+import { fileURLToPath, URL } from 'node:url'
 
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/Mr.-BitCoin---Vue/" : "/",
+})
